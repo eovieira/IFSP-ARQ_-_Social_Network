51ec8cbdfe9b9f8aeeaec01340a4331474295e15
Murillo Vieira <105878091+reqqzfofo@users.noreply.github.com>
2025-03-27 19:35:48 -0300

    Flask adicionado

--- socialnetwork/main/main.py
+++ socialnetwork/main/main.py
@@ -1,3 +1,25 @@
-import os
-import flask
-import flask_login
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, login_remembered, logout_user, current_user
+from models import Usuario
+from db import db
+import hashlib
+
+app = Flask(__name__)
+app.secret_key = 'b1b39f3b13f4d82f957ee82b2aff10ae7d5903aa1ab6baa6c77664f667dde823'
+lm = LoginManager(app)
+lm.login_view = 'login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+
+def hash(txt):
+    hash_obj = hashlib.sha256(txt.encode('utf-8'))
+    return hash_obj.hexdigest()
+
+@lm.user_loader
+def user_loader(id):
+    usuario = db.session.query(Usuario).filter_by(id=id).first()
+    return usuario
+
+@app.route('/')
+def home():
+    return render_template('index.html')
