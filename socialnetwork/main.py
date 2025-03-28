851d519516e66a2e807ae17a10603c587dfd3ff0
Murillo Vieira <105878091+reqqzfofo@users.noreply.github.com>
2025-03-28 00:47:28 +0000

    Atualização em "main.py"

--- socialnetwork/main.py
+++ socialnetwork/main.py
@@ -50,7 +50,7 @@ def registrar():
         usuario_existente = db.session.query(Usuario).filter_by(nome=nome).first()
         if usuario_existente:
             return render_template('registrar.html', error="Este nome de usuário já está em uso, tente outro!")
-        if nome == 'SuperAdmin':
+        if nome == 'SuperAdministrador':
             novo_usuario = Usuario(nome=nome, senha=hash(senha), cargo='Administrador')
         else:
             novo_usuario = Usuario(nome=nome, senha=hash(senha), cargo='Usuário')
