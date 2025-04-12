abbac043ac3bb8695f4766b9b8dd5f4b4fbdc4ba
BrandaoRM01 <raulmb231@gmail.com>
2025-04-12 17:30:42 +0000

    adicionado js para botões de responder comentário e comentar

---
+++ socialnetwork/static/topics.js
@@ -0,0 +1,43 @@
+function submitForm(formId) {
+    var form = document.getElementById(formId);
+    if (form) {
+        form.submit();
+    }
+}
+function mostrarFormulario(event, comentarioId) {
+    event.stopPropagation();
+
+    const formularios = document.querySelectorAll('.form-resposta');
+    formularios.forEach(form => {
+        if (form.id !== `responder-${comentarioId}`) {
+            form.style.display = 'none';
+        }
+    });
+
+    const formulario = document.getElementById(`responder-${comentarioId}`);
+    if (formulario.style.display === 'none' || formulario.style.display === '') {
+        formulario.style.display = 'flex';
+        formulario.querySelector('input[name="texto"]').focus();
+    } else {
+        formulario.style.display = 'none';
+    }
+}
+
+function mostrarFormularioComentario(event, publicacaoId) {
+    event.stopPropagation();
+
+    const formularios = document.querySelectorAll('.form-comentario');
+    formularios.forEach(form => {
+        if (form.id !== `comentar-${publicacaoId}`) {
+            form.style.display = 'none';
+        }
+    });
+
+    const formulario = document.getElementById(`comentar-${publicacaoId}`);
+    if (formulario.style.display === 'none' || formulario.style.display === '') {
+        formulario.style.display = 'flex';
+        formulario.querySelector('input[name="texto"]').focus();
+    } else {
+        formulario.style.display = 'none';
+    }
+}
